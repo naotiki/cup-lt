9f9659981982c38b91a3fc83e073ea08b4fc7ca0
Naotiki <naotiki@naotiki.me>
2024-12-19 00:32:55 +0900

    hot reload

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -3,6 +3,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import io.sellmair.evas.Events
+import io.sellmair.evas.States
+import io.sellmair.evas.compose.installEvas
 import net.kodein.cup.Presentation
 import net.kodein.cup.Slides
 import net.kodein.cup.cupApplication
@@ -14,43 +17,54 @@ import org.kodein.emoji.compose.EmojiService
 import slides.*
 
 
-fun main() = cupApplication(
-    // TODO: Change title
-    title = "Kotlinでスライドを作ろう！"
-) {
-    remember {
-        // https://github.com/kosi-libs/Emoji.kt?tab=readme-ov-file#initializing-the-emoji-service
-        EmojiService.initialize()
-    }
+fun main() {
 
-    MaterialTheme(
-        // TODO: Apply your theme
-        colorScheme = darkColorScheme(),
-        typography = SlideTypography().cupScaleDown()
+    val events = Events()
+    val states = States()
+    cupApplication(
+        // TODO: Change title
+        title = "Kotlinでスライドを作ろう！"
     ) {
-        Presentation(
-            slides = presentationSlides,
-            configuration = {
-                // TODO: Configure plugins
-                speakerWindow()
-                laser()
-            },
-            backgroundColor = MaterialTheme.colorScheme.background
-        ) { slidesContent ->
-            CompositionLocalProvider(
-                LocalContentColor provides MaterialTheme.colorScheme.onBackground
-            ) {
-                slidesContent()
+        installEvas(events, states) {
+            DevelopmentEntryPoint {
+                // Hot reload用にここに書かないといけない
+                val presentationSlides = remember {
+                    // TODO: Write your own slides!
+                    Slides(
+                        preIntro,
+                        intro,
+                        whatIsCuP,
+                        /*amazingHtml*/
+                    )
+                }
+                remember {
+                    // https://github.com/kosi-libs/Emoji.kt?tab=readme-ov-file#initializing-the-emoji-service
+                    EmojiService.initialize()
+                }
+                MaterialTheme(
+                    // TODO: Apply your theme
+                    colorScheme = darkColorScheme(),
+                    typography = SlideTypography().cupScaleDown()
+                ) {
+                    Presentation(
+                        slides = presentationSlides,
+                        configuration = {
+                            // TODO: Configure plugins
+                            speakerWindow()
+                            laser()
+                        },
+                        backgroundColor = MaterialTheme.colorScheme.background
+                    ) { slidesContent ->
+                        CompositionLocalProvider(
+                            LocalContentColor provides MaterialTheme.colorScheme.onBackground
+                        ) {
+                            slidesContent()
+                        }
+                    }
+                }
             }
         }
     }
-
 }
 
-// TODO: Write your own slides!
-val presentationSlides = Slides(
-    preIntro,
-    intro,
-    whatIsCuP,
-    amazingHtml
-)
+
